e28ef8aca9dbad92101daf83dc7a5c123f3b3bb3
Nadilson <nadilson@protonmail.com>
2023-01-24 11:53:19 -0300

    Ajustes na navegação

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -7,6 +7,7 @@ import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 
 import menuImg from '../../assets/menu.svg'
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function SideBar() {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -23,31 +24,31 @@ export function SideBar() {
     <nav>
       <ul>
         <li>
-          <a href='#' className='active'>
+          <NavLink to='/' className='active'>
             <BurgerIcon />
             <span>Hambúrgueres</span>
-          </a>
+          </NavLink>
         </li>
 
         <li>
-          <a href='#'>
+          <NavLink to='pizzas'>
             <PizzaIcon />
             <span>Pizzas</span>
-          </a>
+          </NavLink>
         </li>
 
         <li>
-          <a href='#'>
+          <NavLink to='drinks'>
             <SodaPopIcon />
             <span>Bebidas</span>
-          </a>
+          </NavLink>
         </li>
 
         <li>
-          <a href='#'>
+          <NavLink to='ice-creams'>
             <IceCreamIcon />
             <span>Sorvetes</span>
-          </a>
+          </NavLink>
         </li>
 
     </ul>
