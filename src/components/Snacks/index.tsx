56107a8479c2a6b4de5951ed913c791ccffb071b
Nadilson <nadilson@protonmail.com>
2023-02-11 15:51:10 -0300

    Grid de itens, formatação no componente Snacks e ajustes em outros snacks

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,4 +1,5 @@
 import { Container } from './styles'
+import { FiPlus } from 'react-icons/fi'
 
 interface SnacksProps {
   snacks: any[] // any não é boa prática e está aqui por enquanto, até a definição do tipo da Props
@@ -8,13 +9,15 @@ export function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
       {snacks.map((snack) => (
-        <div key={snack.id}>
+        <div key={snack.id} className='snack'>
           <h2>{snack.name}</h2>
           <img src={snack.image} alt={snack.name} />
           <p>{snack.description}</p>
           <div>
             <strong>{snack.price}</strong>
-            <button type='button'></button>
+            <button type='button'>
+              <FiPlus />
+            </button>
           </div>
         </div>
       ))}
