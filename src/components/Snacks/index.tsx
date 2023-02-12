ed42eaf3bebd2d1160aed91f387eb6abb9d25f4c
Nadilson <nadilson@protonmail.com>
2023-02-11 21:35:52 -0300

    Estrutura do componente de loading e ajustes nas propriedades de types

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from './styles'
 import { FiPlus } from 'react-icons/fi'
 import { currencyFormat } from '../../helpers/currencyFormat'
+import { SkeletonSnack } from './SkeletonSnack'
 
 interface SnacksProps {
   snacks: any[] // any não é boa prática e está aqui por enquanto, até a definição do tipo da Props
@@ -9,23 +10,21 @@ interface SnacksProps {
 export function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
-      {!snacks.length ? (
-        <p style={{ color: '#fff' }}>Carregando...</p>
-      ) : (
-        snacks.map((snack) => (
-          <div key={snack.id} className='snack'>
-            <h2>{snack.name}</h2>
-            <img src={snack.image} alt={snack.name} />
-            <p>{snack.description}</p>
-            <div>
-              <strong>{currencyFormat(snack.price)}</strong>
-              <button type='button'>
-                <FiPlus />
-              </button>
+      {!snacks.length
+        ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+        : snacks.map((snack) => (
+            <div key={snack.id} className='snack'>
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currencyFormat(snack.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
             </div>
-          </div>
-        ))
-      )}
+          ))}
     </Container>
   )
 }
