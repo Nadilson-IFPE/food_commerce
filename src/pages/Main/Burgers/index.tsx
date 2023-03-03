e18eebacac10de3fcd806278f8200d178c98beb9
Nadilson <nadilson@protonmail.com>
2023-03-02 23:48:26 -0300

    Ajustando o projeto com SnackData

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react'
 import { Head } from '../../../components/Head'
 import { Snacks } from '../../../components/Snacks'
 import { SnackTitle } from '../../../components/SnackTitle'
+import { SnackData } from '../../../interfaces/SnackData'
 import { getBurgers } from '../../../services/api'
 
 export default function Burgers() {
-  const [burgers, setBurgers] = useState([])
+  const [burgers, setBurgers] = useState<SnackData[]>([])
+
 
   useEffect(() => {
-    ;(async () => {
+    ; (async () => {
       const burgerRequest = await getBurgers()
 
       setBurgers(burgerRequest.data)
