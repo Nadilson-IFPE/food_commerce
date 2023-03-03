2fbd707bf8f53c9d9a023da837f7a593e0d9666a
Nadilson <nadilson@protonmail.com>
2023-03-02 23:22:13 -0300

    Definindo SnackData

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -2,9 +2,11 @@ import { Container } from './styles'
 import { FiPlus } from 'react-icons/fi'
 import { currencyFormat } from '../../helpers/currencyFormat'
 import { SkeletonSnack } from './SkeletonSnack'
+import { SnackData } from '../../interfaces/SnackData'
 
 interface SnacksProps {
-  snacks: any[] // any não é boa prática e está aqui por enquanto, até a definição do tipo da Props
+  //  snacks: any[] // any não é boa prática e está aqui por enquanto, até a definição do tipo da Props
+  snacks: SnackData[]
 }
 
 export function Snacks({ snacks }: SnacksProps) {
@@ -13,18 +15,18 @@ export function Snacks({ snacks }: SnacksProps) {
       {!snacks.length
         ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
         : snacks.map((snack) => (
-            <div key={snack.id} className='snack'>
-              <h2>{snack.name}</h2>
-              <img src={snack.image} alt={snack.name} />
-              <p>{snack.description}</p>
-              <div>
-                <strong>{currencyFormat(snack.price)}</strong>
-                <button type='button'>
-                  <FiPlus />
-                </button>
-              </div>
+          <div key={snack.id} className='snack'>
+            <h2>{snack.name}</h2>
+            <img src={snack.image} alt={snack.name} />
+            <p>{snack.description}</p>
+            <div>
+              <strong>{currencyFormat(snack.price)}</strong>
+              <button type='button'>
+                <FiPlus />
+              </button>
             </div>
-          ))}
+          </div>
+        ))}
     </Container>
   )
 }
