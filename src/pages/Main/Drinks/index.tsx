417b65666ad2cd64c3fd15f9a75076b9a1e2c898
Nadilson <nadilson@protonmail.com>
2023-04-12 22:11:23 -0300

    Hook useSnack

--- src/pages/Main/Drinks/index.tsx
+++ src/pages/Main/Drinks/index.tsx
@@ -1,11 +1,10 @@
-import { useContext } from 'react'
+import { useSnack } from '../../../hooks/useSnack'
 import { Head } from '../../../components/Head'
 import { Snacks } from '../../../components/Snacks'
 import { SnackTitle } from '../../../components/SnackTitle'
-import { SnackContext } from '../../../contexts/SnackContext'
 
 export default function Drinks() {
-  const { drinks } = useContext(SnackContext)
+  const { drinks } = useSnack()
 
   return (
     <>
